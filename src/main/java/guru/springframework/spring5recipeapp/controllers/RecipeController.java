a6ed57e65c5af948c600622ca2bc758a7046321e
fatih.kose <fatih.kose@hititcs.com>
2022-03-02 20:14:05 +0300

    update recipe feature

--- src/main/java/guru/springframework/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/guru/springframework/spring5recipeapp/controllers/RecipeController.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring5recipeapp.controllers;
 
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import guru.springframework.spring5recipeapp.commands.RecipeCommand;
 import guru.springframework.spring5recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
@@ -8,7 +9,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class RecipeController {
@@ -41,6 +41,12 @@ public class RecipeController {
   public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
     RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
     return "redirect:/recipe/show/" + savedCommand.getId();
+  }
+
+  @RequestMapping("recipe/{id}/update")
+  public String updateRecipe(@PathVariable String id, Model model) {
+    model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
+    return "recipe/recipeform";
   }
 
 }
